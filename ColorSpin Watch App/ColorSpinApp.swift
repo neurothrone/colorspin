bc25563ee95494eaa115d248144cea6be7add854
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-14 17:54:11 +0200

    Initial Commit

---
+++ ColorSpin Watch App/ColorSpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSpinApp.swift
+//  ColorSpin Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-14.
+//
+
+import SwiftUI
+
+@main
+struct ColorSpin_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
